c3b2153f39c69f1ee167b659617797c3103f10df
putee <puyi.novariqa@gmail.com>
2025-09-20 23:17:49 +0700

    Commit No. 1 – Navigation drawer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.4")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
